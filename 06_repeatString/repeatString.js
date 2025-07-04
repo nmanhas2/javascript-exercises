e8494d4a1ce5dad0cb9bc48a604f1ed24dd4c58d
nmanhas2 <manhasnubal@gmail.com>
2025-07-03 19:27:17 -0600

    completed exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
+    if (repeat < 0) return "ERROR";
+    let str = "";
+    for(let i = 0; i < repeat; i++){
+        str += string;
+    }
 
+    return str;
 };
 
 // Do not edit below this line
