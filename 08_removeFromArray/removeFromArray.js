2e04e3c43e10a79919f72e5fa614bb15f2231f87
nmanhas2 <manhasnubal@gmail.com>
2025-07-03 20:03:10 -0600

    Completed exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...args) {
+    let removed = array.filter((x) =>{
+        return !args.includes(x);
+    });
+    return removed;
 };
 
 // Do not edit below this line
